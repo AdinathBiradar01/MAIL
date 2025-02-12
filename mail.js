1d93ac05e581124616d447439b131bc2d168fa0b
AdinathBiradar01 <adinath.biradar@mitcorer.edu.in>
2025-02-12 15:24:07 +0530

    Initial commit

---
+++ mail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter
+const transporter = nodemailer.createTransport({
+  service: 'yahoo', // Use a service like 'hotmail', 'yahoo', etc.
+  auth: {
+    user: 'biradaradinath2gmail.com',  // Your email address
+    pass: 'amqnfyifcxmzwfhm'    // Your email password or app-specific password
+  }
+});
+
+// Email options
+const mailOptions = {
+  from: 'biradaradinath2@gmail.com',
+  to: 'biradaradinath3@gmail.com',
+  subject: 'Hello from Node.js',
+  text: 'This is a test email sent using Nodemailer.'
+};
+
+// Send the email
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    console.log('Email sent:', info.response);
+  }
+});
